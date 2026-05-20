07368967aa9b2fe37256767049ced94f573d0888
JJJ320 <gabrizao122@gmail.com>
2026-05-20 16:35:59 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log("Site iniciado!")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyB1eFWxsEoKzksJ43kKZcr4GosmwU4etWw",
+
+  authDomain: "site-clube-escolar-ael.firebaseapp.com",
+
+  projectId: "site-clube-escolar-ael",
+
+  storageBucket: "site-clube-escolar-ael.firebasestorage.app",
+
+  messagingSenderId: "358819384973",
+
+  appId: "1:358819384973:web:788dba83f5bf4eb3b49cc4",
+
+  measurementId: "G-Q2LYVXJJ47"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+
+const loginBtn = document.getElementById("loginBtn");
+
+const userDiv = document.getElementById("user");
+
+
+loginBtn.addEventListener("click", async () => {
+
+  try {
+
+    const result = await signInWithPopup(auth, provider);
+
+    const user = result.user;
+
+    userDiv.innerHTML = `
+
+      <h2>${user.displayName}</h2>
+
+      <img src="${user.photoURL}" width="100" style="border-radius:50%">
+
+    `;
+
+  } catch(error){
+
+    console.error(error);
+
+  }
+
+});
